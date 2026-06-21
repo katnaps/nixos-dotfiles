b6f7b12e4d4bb6c5d820582b813c694ca74afaab
katnaps <59246759+katnaps@users.noreply.github.com>
2026-06-22 02:59:11 +0800

    adding files

---
+++ .zshrc
@@ -0,0 +1,55 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.history_zsh
+HISTSIZE=10000
+SAVEHIST=10000
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/coconut/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+#
+#
+#
+### alias list
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# alias fanspeed=$HOME/scripts/fanSpeed
+alias cpustatus=$HOME/scripts/readCpuGovernor
+alias setcpu=$HOME/scripts/setCpuPerformance
+
+# Connect to bluetooth audio
+alias audiobluetooth='bluetoothctl connect F4:4E:FD:49:F8:3C'
+
+# Disconnect bluetooth connection
+alias disconnectBluetooth='bluetoothctl disconnect'
+
+# Check bluetooth battery percentage
+alias batterybluetooth='bluetoothctl info | grep -E "Name|Battery"'
+
+# Function section
+# Check fan speed
+function fanspeed {
+	bash $HOME/scripts/fanSpeed "$@"
+}
+
+# Check PlayStation Controller Battery
+function ds4Battery {
+	bash $HOME/scripts/ps4BatteryCheck "$@"
+}
+
+# Prompt
+eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/p10k.toml)"
+
+# Zoxide
+eval "$(zoxide init --cmd cd zsh)"
+
+# Exports
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+# fzf Integratration
+source <(fzf --zsh)
---
+++ configuration.nix
@@ -0,0 +1,141 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.blacklistedKernelModules = [ "iwlwifi" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kuala_Lumpur";
+
+  nixpkgs.config = {
+      allowUnfree = true;
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.displayManager.ly.enable = true;
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+
+  programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.coconut = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    foot
+    git
+    waybar
+    kitty
+    wiremix
+    rofi
+    swayimg
+    usbutils
+    fastfetch
+    zoxide
+  ];
+
+  fonts.packages = with pkgs; [
+    fira-code
+    nerd-fonts.jetbrains-mono
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
+
