ee17ec89fe0812042ac768a16077060728238392
katnaps <59246759+katnaps@users.noreply.github.com>
2026-06-24 08:30:52 +0800

    hyprland inputs update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,8 +47,8 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   # Enable CUPS to print documents.
