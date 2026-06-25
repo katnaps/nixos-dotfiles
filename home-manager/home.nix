ed87541a83012c43b0cd2b30387690969cccf5ca
katnaps <59246759+katnaps@users.noreply.github.com>
2026-06-25 22:41:15 +0800

    getting udiskie to startup with hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,7 +4,6 @@ let
   dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
   configs = {
-    hypr = "hypr";
     foot = "foot";
     waybar = "waybar";
     rofi = "rofi";
@@ -16,22 +15,19 @@ in
   home.username = "coconut";
   home.homeDirectory = "/home/coconut";
 
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #
-  #   # This is the key part for proper service startup (udiskie, etc.)
-  #   systemd = {
-  #     enable = true;
-  #     variables = ["--all"];
-  #   };
-  #
-  #   # Since you're managing config with symlink, tell HM not to generate its own
-  #   configType = "lua";        # or "none" depending on your HM version
-  # };
-  #
-  # home.file."hyprland.lua" = {
-  #   source = create_symlink "${config.home.homeDirectory}/nixos-dotfiles/config/hypr/hyprland.lua";
-  # };
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    # This is the key part for proper service startup (udiskie, etc.)
+    systemd = {
+      enable = true;
+      variables = ["--all"];
+    };
+
+    # Since you're managing config with symlink, tell HM not to generate its own
+    configType = "lua";        # or "none" depending on your HM version
+    extraConfig = builtins.readFile ../config/hypr/hyprland.lua;
+  };
 
   home.file.".zshrc" = {
     source = create_symlink "${config.home.homeDirectory}/nixos-dotfiles/.zshrc";
