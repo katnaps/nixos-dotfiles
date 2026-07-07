f9bf8c3f1ccd3b7a0407c13dcf28d70acdfc4b56
katnaps <59246759+katnaps@users.noreply.github.com>
2026-07-07 22:43:49 +0800

    updated intel updateMicrocode

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -41,7 +41,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # Install microcode for intel CPU
+  # Install microcode for Intel CPU
   hardware.cpu.intel.updateMicrocode = true;
 
   services.displayManager.ly.enable = true;
