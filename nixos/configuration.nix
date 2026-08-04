0e5666d8620e5b6e1a3c07e6112c3a2f82249560
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-04 20:59:01 +0800

    change hyprland from development build to nixpkgs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,20 +22,9 @@
 
   nix.settings = {
     auto-optimise-store = true;
-
     experimental-features = [
       "nix-command"
       "flakes"
-    ];
-
-    extra-substituters = [ "https://hyprland.cachix.org" ];
-    extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-
-    trusted-users = [
-      "root"
-      "@wheel"
     ];
   };
 
@@ -67,9 +56,6 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
