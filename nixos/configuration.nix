434e1089dc13e7c7c620e4a93d563258b1ccc99b
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-04 20:11:30 +0800

    update configuration.nix with nix.settings and use cachix for hyprland binary

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,25 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   nix.optimise.automatic = true;
-  nix.settings.auto-optimise-store = true;
+
+  nix.settings = {
+    auto-optimise-store = true;
+
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
 
   nix.gc = {
     automatic = true;
@@ -119,11 +137,6 @@
     liberation_ttf
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
-  ];
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
   ];
 
   # List services that you want to enable:
