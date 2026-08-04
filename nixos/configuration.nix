c40f08d16467323bd7d2836292d3a35688c7ee02
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-04 22:43:14 +0800

    switch hyprland development build to nixpkgs for flake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,14 +26,6 @@
       "nix-command"
       "flakes"
     ];
-    extra-substituters = [ "https://hyprland.cachix.org" ];
-    extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
   };
 
   nix.gc = {
@@ -64,9 +56,6 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
