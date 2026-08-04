b31c2a968ec39bde7722367ce0894ba683a9efdd
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-04 22:30:28 +0800

    uncomment hyprland packages in nixos/configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -64,9 +64,9 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # portalPackage =
-    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
