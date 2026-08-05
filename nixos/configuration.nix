ed93e2fd04280c79667c358942d59733d8713797
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-06 04:12:27 +0800

    removed root from nix.settings.trusted-users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -30,10 +30,7 @@
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
+    trusted-users = [ "@wheel" ];
   };
 
   nix.gc = {
