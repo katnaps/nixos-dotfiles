d0f1ae9fc63a0d18a45d38200c7d1510a229d630
katnaps <59246759+katnaps@users.noreply.github.com>
2026-08-06 18:40:59 +0800

    update format for nix.settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,6 +26,7 @@
       "nix-command"
       "flakes"
     ];
+
     extra-substituters = [ "https://hyprland.cachix.org" ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
